85460de9d27501d510ccdd48edd821efb17d643e
Olivier Korach <olivier.korach@gmail.com>
2016-10-13 09:57:33 +0200

    First HTML5 version

---
+++ bio_olivier.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.6.1, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.6.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/volleyball-96-1-128x128-21.png" type="image/x-icon">
+  <meta name="description" content="Club de Volley de La Bridoire dans l'avant pays savoyard / bassin du lac d'Aiguebelette
+(La Bridoire, Dullin, Ayn, Rochefort, St Alban de Montbel, Lepin le lac, Attignat-Oncin, La Bauche, Les Echelles, Pont de Beauvoisin, Verel de Montbel, Novalaise, Entre-deux-guiers...)">
+  <title>Volley La Bridoire</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,100,200,300,500,600,700,800,900">
+  <link rel="stylesheet" href="assets/et-line-font-plugin/style.css">
+  <link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/socicon.min.css">
+  <link rel="stylesheet" href="assets/animate.css/animate.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85050755-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- /Google Analytics -->
+
+<section id="menu-0">
+
+    <nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
+        <div class="container">
+
+            <div class="mbr-table">
+                <div class="mbr-table-cell">
+
+                    <div class="navbar-brand">
+                        <a href="index.html" class="navbar-logo"><img src="assets/images/volleyball-96-1-128x128-21.png" alt="Volley La Bridoire"></a>
+                        <a class="navbar-caption" href="https://volley.bridoire.free.fr">VOLLEY LA BRIDOIRE</a>
+                    </div>
+
+                </div>
+                <div class="mbr-table-cell">
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">
+						<li class="nav-item"><a class="nav-link link" href="contacts.html">Nous contacter</a></li>
+						<li class="nav-item"><a class="nav-link link" href="gymnase.html">Nous retrouver</a></li>
+						<li class="nav-item"><a class="nav-link link" href="photos.html">Photos</a></li>
+						<li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="index.html" aria-expanded="false">Autres</a>
+							<div class="dropdown-menu">
+								<a class="dropdown-item" href="gymnase.html">Nous retrouver</a>
+								<a class="dropdown-item" href="contacts.html">Nous contacter</a>
+								<a class="dropdown-item" href="joueurs.html">Joueurs</a>
+								<a class="dropdown-item" href="photos.html">Photos</a>
+							</div>
+						</li>
+						<li class="nav-item nav-btn"><a class="nav-link btn btn-white btn-white-outline" href="index.html">HOME</a></li></ul>
+                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="close-icon"></div>
+                    </button>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</section>
+<section class="mbr-section mbr-parallax-background" id="page4-msg-box7-0" style="background-image: url(photos/backgrounds/bg_joueurs.jpg); padding-top: 240px; padding-bottom: 240px;">
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(0, 0, 0);">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="mbr-table-md-up">
+              <div class="mbr-table-cell mbr-right-padding-md-up mbr-valign-top col-md-6">
+                  <div class="mbr-figure" ><img src="photos/joueurs/olivier.jpg"></div>
+              </div>
+              <div class="mbr-table-cell oko-player col-md-6 text-xs-center text-md-left">
+                  <h3 class="mbr-section-title display-2">Olivier</h3>
+                  <div class="lead">
+                    <p>Surnom: Le Président Tout Puissant<br>
+					Poste préféré: Réceptionneur Attaquant<br>
+					Autres postes: Passeur, Relanceur<br>
+					Quoi d'autre: Se transforme en une bête parfois pénible sur le terrain</p>
+
+					<?php include 'joueurs_liste.html';?>
+
+                  </div>
+              </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="footer1-0" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+    
+    <div class="container">
+        <p class="text-xs-center"><a href="index.html">Home</a> |
+		<?php include 'joueurs_liste.html';?>	
+	
+		<br>Copyright (c) 2016 Volley La Bridoire.</p>
+    </div>
+</footer>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/smooth-scroll/SmoothScroll.js"></script>
+  <script src="assets/viewportChecker/jquery.viewportchecker.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <script src="assets/jarallax/jarallax.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>

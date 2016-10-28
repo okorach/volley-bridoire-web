f3e6e11d73c197a352bf713cf32e83e907d4adc6
Olivier Korach <olivier.korach@sonarsource.com>
2016-10-28 17:02:12 +0200

    Removed unecessary intermediate page title

--- news.php
+++ news.php
@@ -22,26 +22,13 @@ $nextyear = $saison+1;
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-xs-center">
-                <h3 class="mbr-section-title display-2">News</h3>
-                <div class="lead">Quoi de neuf au club ?</div>
-                
-            </div>
-        </div>
-    </div>
-
-</section>
-
-<section class="mbr-section mbr-section__container" id="header3-0" style="background-color: rgb(239, 239, 239); padding-top: 20px; padding-bottom: 20px;">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
 <?php
-	echo "<h3 class=\"mbr-section-title display-2\">Saison $saison/$nextyear</h3>";
-?>
-                
+               echo "<h3 class=\"mbr-section-title display-2\">News Saison $saison/$nextyear</h3>";
+?>           
             </div>
         </div>
     </div>
+
 </section>
 
 <section class="mbr-section article mbr-section__container" id="content1-0" style="background-color: rgb(204, 204, 204); padding-top: 20px; padding-bottom: 20px;">
@@ -57,15 +44,15 @@ rsort($files);
 
 foreach ($files as $file)
 {
-	if (preg_match('/^news_(\d\d\d\d)-(\d\d)-\d\d\.html$/', $file, $matches) )
-	{
-		if (($matches[1] == $saison && $matches[2] >= 8) || ($matches[1] == ($saison+1) && $matches[2] <= 7))
-		{
-			echo ' <div class="container"> <div class="row"> <div class="col-xs-12 lead"><p>';
-			include("news/".$file);
-			echo "</div> </div> </div> <p></p>\n";
-		}
-	}
+   if (preg_match('/^news_(\d\d\d\d)-(\d\d)-\d\d\.html$/', $file, $matches) )
+   {
+      if (($matches[1] == $saison && $matches[2] >= 8) || ($matches[1] == ($saison+1) && $matches[2] <= 7))
+      {
+         echo ' <div class="container"> <div class="row"> <div class="col-xs-12 lead"><p>';
+         include("news/".$file);
+         echo "</div> </div> </div> <p></p>\n";
+      }
+   }
 }
 ?>
 
@@ -90,35 +77,9 @@ foreach ($files as $file)
 
 </section>
 
-<section class="mbr-section mbr-section-md-padding mbr-footer footer1" id="contacts1-0" style="background-color: rgb(46, 46, 46); padding-top: 90px; padding-bottom: 90px;">
-    
-    <div class="container">
-        <div class="row">
-            <div class="mbr-footer-content col-xs-12 col-md-3">
-                <div><a href="http://www.fivb.org" target="_blank"><img src="assets/images/volleyball-128x197-64.png" alt="FIVB" title="FIVB"></a></div>
-            </div>
-            <div class="mbr-footer-content col-xs-12 col-md-3">
-                <p><strong>Adresse</strong><br><strong><br></strong>Volley La Bridoire<span style="font-size: 0.875rem; line-height: 1.5;">c/o Olivier Korach&nbsp;</span>1010 rte des champs du mont&nbsp;73520 La Bridoire</p>
-            </div>
-            <div class="mbr-footer-content col-xs-12 col-md-3">
-                <p><font color="#7c7c7c" face="Montserrat, sans-serif" size="3"><span style="letter-spacing: -1px; line-height: 20px;"><strong>Contacts</strong></span></font><br>
-<br>Email: <a href="mailto:volley.bridoire@free.fr" target="_blank">volley.bridoire@free.fr</a><br>
-Tel: <a href="tel:+33689555081" target="_blank">06 89 55 50 81</a><br></p>
-            </div>
-            <div class="mbr-footer-content col-xs-12 col-md-3">
-                <p></p><p>Contacts</p><p><br>
-<a href="http://www.fsgt73.fr" target="_blank">FSGT 73</a><br>
-<a href="https://www.volley.asso.fr">FFVB</a><br>
-<a href="http://www.fivb.org" target="_blank">FIVB</a></p><a href="http://www.fivb.org/EN/Volleyball/Rules/Rules.htm">Regles du Volley</a><p></p>
-            </div>
+   <?php include 'inc/footer.html';?>
 
-        </div>
-    </div>
-</section>
-
-	<?php include 'inc/footer.html';?>
-
-	<?php include 'inc/jsscripts.html';?>
+   <?php include 'inc/jsscripts.html';?>
 
   <input name="animation" type="hidden">
   </body>

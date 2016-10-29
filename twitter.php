70609aadb3d8be2df297902c37190f5bc351bff9
Olivier Korach <olivier.korach@sonarsource.com>
2016-10-29 11:27:58 +0200

    Ajout feed twitter

---
+++ twitter.php
@@ -0,0 +1,64 @@
+<?php include 'inc/header.html';?>
+
+<body>
+
+<?php include 'inc/analytics.html';?>
+
+<?php include 'inc/menubar.html';?>
+
+<section class="mbr-section mbr-section--relative mbr-section--fixed-size" id="twitter-feed-block-1" style="background-color: rgb(255, 255, 255);">
+            
+        <div class="row">
+            <div class="col-xs-12 col-md-6 lead">
+               <div class="container">
+                   <div class="mbr-section__container mbr-section__container--isolated">
+                       <div class="twitterFeed text-center"><a class="twitter-timeline" href="https://twitter.com/volleybridoire" data-widget-id="670227744614187012" data-screen-name="volleybridoire" data-theme="light" width="520" height="600">Tweets by @VolleyBridoire</a></div>
+                   </div>	
+			      </div>
+		     </div>
+            <div class="col-xs-12 col-md-6 lead">
+               <div class="container">
+                   <div class="mbr-section__container mbr-section__container--isolated">
+                        <div class="twitterFeed text-center"><a class="twitter-timeline" href="https://twitter.com/mobirise" data-widget-id="670227744614187012" data-screen-name="mobirise" data-theme="light" width="520" height="600">Tweets by @mobirise</a></div>
+                   </div>
+               </div>
+            </div>	
+        </div>
+</section>
+
+<section class="mbr-section mbr-section-md-padding mbr-after-navbar" id="page3-social-buttons1-0" style="background-color: rgb(255, 255, 255); padding-top: 90px; padding-bottom: 90px;">
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2 text-xs-center">
+                <h3 class="mbr-section-title display-2">Partagez cette page!</h3>
+                <div>
+
+                  <div class="mbr-social-likes" data-counters="false">
+                    <span class="btn btn-social facebook" title="Share link on Facebook">
+                        <i class="socicon socicon-facebook"></i>
+                    </span>
+                    <span class="btn btn-social twitter" title="Share link on Twitter">
+                        <i class="socicon socicon-twitter"></i>
+                    </span>
+                    <span class="btn btn-social plusone" title="Share link on Google+">
+                        <i class="socicon socicon-google"></i>
+                    </span>
+                    
+                    
+                  </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+	<?php include 'inc/footer.html';?>
+
+	<?php include 'inc/jsscripts.html';?>
+   <script src="https://platform.twitter.com/widgets.js"></script>
+  
+  <input name="animation" type="hidden">
+  </body>
+</html>

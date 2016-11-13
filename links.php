c2b0dbf7a29d4e0f24ea2a78133da454e5b187d1
Olivier Korach <olivier.korach@sonarsource.com>
2016-11-13 15:04:34 +0100

    Separate link data and code

--- links.php
+++ links.php
@@ -1,3 +1,63 @@
+<?php
+
+class Link {
+	var $title;
+	var $url;
+	function Link($title, $url) {
+		$this->title = $title;
+		$this->url = $url;
+	}
+	function getTitle() { return $this->title; }
+	function getUrl() { return $this->url; }
+	function compare($otherLink) {
+		return strcmp($this->title, $otherLink->getTitle());
+	}
+   function ucompare($a, $b) {
+      return $a->compare($b);
+	}
+}
+
+class LinkList
+{
+	var $links;
+   function LinkList($csvfile)
+   {
+		$fh = fopen($csvfile, "r");
+		while (false !== ($line = fgets($fh))) {
+         $data = preg_split('/\s*,\s*/', $line);
+         $this->links[] = array($data[0], new Link($data[1], $data[2]));
+      }
+      fclose($fh);
+   }
+	/*
+	function sort() {
+		usort($this, 'Link::ucompare');
+	}
+	*/
+   
+   function getTypes()
+   {
+      foreach ($this->links as $link) {
+         $types[$link[0]]++;
+      }
+      return $types;      
+   }
+
+   function getLinksByType($type)
+   {
+      foreach ($this->links as $link) {
+         if ($link[0] == $type) { $linksOfType[] = $link[1]; }
+      }
+      return $linksOfType;
+   }
+
+   function count()
+   {
+      return count($links);
+   }
+}
+?>
+
 <?php include 'inc/header.html';?>
 
 <body>
@@ -11,31 +71,45 @@
     <div class="container">
         <div class="row">
             <div class="mbr-footer-content col-xs-12 col-md-3">
-                <div><img src="assets/images/logo.png"></div>
+                <div><img src="logo400x400.jpg" width="200" height="200" style="-webkit-border-radius: 20px 20px 20px 20px;-moz-border-radius: 20px 20px 20px 20px;border-radius: 20px 20px 20px 20px;-webkit-box-shadow: #B3B3B3 8px 8px 8px;-moz-box-shadow: #B3B3B3 8px 8px 8px; box-shadow: #B3B3B3 8px 8px 8px;">
+					 </div>
             </div>
             <div class="mbr-footer-content col-xs-12 col-md-3">
-                <p><strong>Sites des autres clubs de Volley FSGT Savoie</strong><br>
-<a class="text-primary" href="http://volleyclubbassens73.e-monsite.com/" target="_blank">VC Bassens</a><br>
-<a class="text-primary" href="http://touvetvolleyball.blogspot.fr/" target="_blank">Touvet VB</a><br>
-<a class="text-primary" href="http://asvolleysaintgenixaoste.clubeo.com/" target="_blank">AS St Genix / Aoste</a><br>
-<a class="text-primary" href="https://sites.google.com/site/volleyballpont/" target="_blank">VB Pont de Beauvoisin</a><br>
-<a class="text-primary" href="http://www.volleyclub-bourg.com/" target="_blank">VC Bourg St Maurice</a><br>
-A venir<br>
-</p>
-            </div>
-            <div class="mbr-footer-content col-xs-12 col-md-3">
-                <p><strong>Sites li&eacute;s volley</strong><br>
-<a class="text-primary" href="http://www.fivb.org/EN/Volleyball/Rules/Rules.htm" target="_blank">R&egrave;gles du volley</a><br>
-<a class="text-primary" href="http://www.volley.asso.fr" target="_blank">FFVB</a><br>
-<a class="text-primary" href="http://www.fsgt73.fr">FSGT Savoie</a></p>
-            </div>
-            <div class="mbr-footer-content col-xs-12 col-md-3">
-                <p><strong>Sites de f&eacute;d&eacute;rations</strong><br>
-<a class="text-primary" href="http://www.fivb.org" target="_blank">FIVB</a><br>
-<a class="text-primary" href="http://www.volley.asso.fr" target="_blank">FFVB</a><br>
-<a class="text-primary" href="http://www.fsgt73.fr">FSGT Savoie</a></p>
-            </div>
+               <p><strong>Sites des autres clubs de Volley FSGT Savoie</strong>
 
+               <?php
+                  $list = new LinkList('links.csv');
+                  $links = $list->getLinksByType('Club');
+                  foreach ($links as $link)
+                  {
+                     echo '<a class="text-primary" href="'.$link->getUrl().'" target="_blank">'.$link->getTitle().'</a><br>'."\n";
+                  }
+
+                  echo <<<END
+               </p>
+            </div>
+            <div class="mbr-footer-content col-xs-12 col-md-3">
+               <p><strong>Sites de fédérations</strong><br><br>
+END;
+                  $links = $list->getLinksByType('Fede');
+                  foreach ($links as $link)
+                  {
+                     echo '<a class="text-primary" href="'.$link->getUrl().'" target="_blank">'.$link->getTitle().'</a><br>'."\n";
+                  }
+                  echo <<<END
+               </p>
+            </div>
+            <div class="mbr-footer-content col-xs-12 col-md-3">
+                <p><strong>Autres liens</strong><br>
+END;
+                  /* $links = $list->getLinksByType('Autres');
+                  foreach ($links as $link)
+                  {
+                     echo '<a class="text-primary" href="'.$link->getUrl().'" target="_blank">'.$link->getTitle().'</a><br>'."\n";
+                  }
+						*/
+?>
+            </div>
         </div>
     </div>
 </section>

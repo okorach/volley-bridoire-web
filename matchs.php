8b6948827e5bb1d352d404b979d991deff418b68
Olivier Korach <olivier.korach@sonarsource.com>
2016-11-19 18:37:23 +0100

    Take scores from DB

--- matchs.php
+++ matchs.php
@@ -15,6 +15,16 @@ if (isset($_GET['saison'])) {
 $nextyear = $saison+1;
 ?>
 
+<style>
+div.transbox {
+    margin: auto;
+    background-color: #ffffff;
+    border: 1px solid black;
+    opacity: 0.7;
+    filter: alpha(opacity=70); /* For IE8 and earlier */
+}
+</style>
+
 <section class="mbr-section mbr-parallax-background mbr-after-navbar" id="msg-box8-0" style="background-image: url(assets/images/desert.jpg); padding-top: 80px; padding-bottom: 40px;">
 
     <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(34, 34, 34);">
@@ -31,61 +41,64 @@ $nextyear = $saison+1;
 
 </section>
 
-<style>
-div.transbox {
-    margin: auto;
-    background-color: #ffffff;
-    border: 1px solid black;
-    opacity: 0.7;
-    filter: alpha(opacity=70); /* For IE8 and earlier */
-}
-</style>
-
 <section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background" id="planning_matchs" style="background-image: url(photos/backgrounds/bg_joueur_attack.jpg);">
 
 <?php
+ini_set('display_errors', 1);
+ini_set('log_errors', 1); 
+error_reporting(E_ALL | E_STRICT);
 
-$fh = fopen("matchs.csv", "r");
-while (false !== ($line = fgets($fh))) {
-   $data = preg_split('/\s*,\s*/', trim($line));
-   #print_r($data);
-   $datematch[] = $data[0];
-   $eqdom[] = $data[1];
-   $eqvis[] = $data[2];
-   $score[] = $data[3];
-   $sets[] = $data[4];
+if (! ($link = mysql_connect('sql.free.fr', 'volley.bridoire', 'lhassa73')) ) {
+   echo( '3 Unable to connect to database: '.mysql_error().'"'."\n");
 }
-fclose($fh);
+
+if ( !  mysql_select_db('volley.bridoire') ) {
+   die( 'Unable to select database: "'.mysql_error().'"'."\n");
+}
+$result=mysql_query('select * from matchs');
+
+$n = mysql_numrows($result);
+for ($i = 0; $i < $n; $i++)
+{
+	$a = preg_split("/-/", mysql_result($result,$i,'date_match'));
+   $a[0] = $a[0] % 100;
+   $datematch[] = $a[2]."/".$a[1]."/".$a[0];
+   $eqdom[] = mysql_result($result,$i,'equipe_domicile');
+   $eqvis[] = mysql_result($result,$i,'equipe_visiteur');
+   $score[] = mysql_result($result,$i,'score1');
+	$sets[] = mysql_result($result,$i,'score2');
+}
+mysql_close($link);
 
 echo "<div class=\"scoretable transbox\">\n";
 for ($i=0; $i<count($score); $i++)
 {
-   if ($score[$i] == "") {
-      $res = 'unknown';
-   } else if ($eqdom[$i] == 'VCB') {
-      if (preg_match('/3\//', $score[$i])) {
-         $res = 'win';
-      } else if (preg_match('/2\/3/', $score[$i])) {
-         $res = 'tiebreak';
-      } else {
-         $res = 'loss';
-      }
-   } else {
-      if (preg_match('/\/3/', $score[$i])) {
-         $res = 'win';
-      } else if (preg_match('/3\/2/', $score[$i])) {
-         $res = 'tiebreak';
-      } else {
-         $res = 'loss';
-      }
-   }
-   echo "<div class=\"gamerow\">\n";
-   echo "   <div class=\"scoretablecell gamedate\">$datematch[$i]</div>\n";
-   echo "   <div class=\"scoretablecell team vcb-team\">$eqdom[$i]</div>\n";
-   echo "   <div class=\"scoretablecell score vcb-score $res\" title=\"$sets[$i]\">$score[$i]</div>\n";
-   echo "   <div class=\"scoretablecell team vcb-team\">$eqvis[$i]</div>\n";
-   #echo "   <td>$datematch[$i]<br>$sets[$i]</td>";
-   echo "</div>\n";
+	if ($score[$i] == "") {
+		$res = 'unknown';
+	} else if ($eqdom[$i] == 'VCB') {
+		if (preg_match('/3\//', $score[$i])) {
+			$res = 'win';
+		} else if (preg_match('/2\/3/', $score[$i])) {
+			$res = 'tiebreak';
+		} else {
+			$res = 'loss';
+		}
+	} else {
+		if (preg_match('/\/3/', $score[$i])) {
+			$res = 'win';
+		} else if (preg_match('/3\/2/', $score[$i])) {
+			$res = 'tiebreak';
+		} else {
+			$res = 'loss';
+		}
+	}
+	echo "<div class=\"gamerow\">\n";
+	echo "   <div class=\"scoretablecell gamedate\">$datematch[$i]</div>\n";
+	echo "   <div class=\"scoretablecell team vcb-team\">$eqdom[$i]</div>\n";
+	echo "   <div class=\"scoretablecell score vcb-score $res\" title=\"$sets[$i]\">$score[$i]</div>\n";
+	echo "   <div class=\"scoretablecell team vcb-team\">$eqvis[$i]</div>\n";
+	#echo "   <td>$datematch[$i]<br>$sets[$i]</td>";
+	echo "</div>\n";
 
 }
 echo "</div>\n";
@@ -96,9 +109,9 @@ echo "</div>\n";
 
 </section>
 
-   <?php include 'inc/footer.html';?>
+	<?php include 'inc/footer.html';?>
 
-   <?php include 'inc/jsscripts.html';?>
+	<?php include 'inc/jsscripts.html';?>
    <script src="https://platform.twitter.com/widgets.js"></script>
   
   <input name="animation" type="hidden">

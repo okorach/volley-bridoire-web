18135934e71e02cf96b42f4b7a0953064f40bb87
Olivier Korach <olivier.korach@sonarsource.com>
2016-11-26 22:00:57 +0100

    Game object abstraction

--- matchs.php
+++ matchs.php
@@ -1,10 +1,12 @@
+
 <?php include 'inc/header.html';?>
 
 <body>
 
-<?php include 'inc/analytics.html';?>
-
-<?php include 'inc/menubar.html';?>
+<?php
+include 'inc/analytics.html';
+include 'inc/menubar.html';
+?>
 
 <?php
 if (isset($_GET['saison'])) {
@@ -44,59 +46,30 @@ div.transbox {
 <section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background" id="planning_matchs" style="background-image: url(photos/backgrounds/bg_joueur_attack.jpg);">
 
 <?php
-ini_set('display_errors', 1);
-ini_set('log_errors', 1); 
-error_reporting(E_ALL | E_STRICT);
 
-if (! ($link = mysql_connect('sql.free.fr', 'volley.bridoire', 'lhassa73')) ) {
-   echo( '3 Unable to connect to database: '.mysql_error().'"'."\n");
-}
-
-if ( !  mysql_select_db('volley.bridoire') ) {
-   die( 'Unable to select database: "'.mysql_error().'"'."\n");
-}
-$result=mysql_query('select * from matchs');
-
-$n = mysql_numrows($result);
-for ($i = 0; $i < $n; $i++)
-{
-	$a = preg_split("/-/", mysql_result($result,$i,'date_match'));
-   $a[0] = $a[0] % 100;
-   $datematch[] = $a[2]."/".$a[1]."/".$a[0];
-   $eqdom[] = mysql_result($result,$i,'equipe_domicile');
-   $eqvis[] = mysql_result($result,$i,'equipe_visiteur');
-   $score[] = mysql_result($result,$i,'score1');
-	$sets[] = mysql_result($result,$i,'score2');
-}
-mysql_close($link);
+include 'lib/game.php';
+$gamelist = Game::ReadFromDB();
 
 echo "<div class=\"scoretable transbox\">\n";
-for ($i=0; $i<count($score); $i++)
+foreach ($gamelist as $game)
 {
-	if ($score[$i] == "") {
+	$w = $game->winner();
+	if ($w == '') {
 		$res = 'unknown';
-	} else if ($eqdom[$i] == 'VCB') {
-		if (preg_match('/3\//', $score[$i])) {
-			$res = 'win';
-		} else if (preg_match('/2\/3/', $score[$i])) {
-			$res = 'tiebreak';
-		} else {
-			$res = 'loss';
-		}
+	} elseif ($w == 'VCB') {
+		$res = 'win';
 	} else {
-		if (preg_match('/\/3/', $score[$i])) {
-			$res = 'win';
-		} else if (preg_match('/3\/2/', $score[$i])) {
+		if (preg_match('/(3\/2|2\/3)/', $game->getScore())) {
 			$res = 'tiebreak';
 		} else {
 			$res = 'loss';
 		}
-	}
+	} 
 	echo "<div class=\"gamerow\">\n";
-	echo "   <div class=\"scoretablecell gamedate\">$datematch[$i]</div>\n";
-	echo "   <div class=\"scoretablecell team vcb-team\">$eqdom[$i]</div>\n";
-	echo "   <div class=\"scoretablecell score vcb-score $res\" title=\"$sets[$i]\">$score[$i]</div>\n";
-	echo "   <div class=\"scoretablecell team vcb-team\">$eqvis[$i]</div>\n";
+	echo "   <div class=\"scoretablecell gamedate\">".$game->getDate('%d/%m/%y')."</div>\n";
+	echo "   <div class=\"scoretablecell team vcb-team\">".$game->getHomeTeam()."</div>\n";
+	echo "   <div class=\"scoretablecell score vcb-score $res\" title=\"".$game->getSets()."\">".$game->getScore()."</div>\n";
+	echo "   <div class=\"scoretablecell team vcb-team\">".$game->getAwayTeam()."</div>\n";
 	#echo "   <td>$datematch[$i]<br>$sets[$i]</td>";
 	echo "</div>\n";
 

afcee669ecd627393fc8797ebad6404b3f9d3737
Olivier Korach <olivier.korach@sonarsource.com>
2016-12-25 16:09:47 +0100

    Reduce line length after SonarQube scan

--- contacts.php
+++ contacts.php
@@ -6,9 +6,8 @@
 
 <?php require_once 'inc/menubar.html';?>
 
-<section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background mbr-after-navbar" id="header1-0" style="background-image: url(photos/perso/vcb-2014-2.jpg);">
-
-    
+<section class="mbr-section mbr-section-hero mbr-section-full mbr-parallax-background mbr-after-navbar"
+   id="header1-0" style="background-image: url(photos/perso/vcb-2014-2.jpg);">
 
     <div class="mbr-table-cell">
 
@@ -17,21 +16,24 @@
                 <div class="mbr-section col-md-10 col-md-offset-1 text-xs-center">
 
                     <h1 class="mbr-section-title display-1">Nous contacter</h1>
-                    <p class="mbr-section-lead lead">Vous pouvez nous contacter:<br>Par email: <strong>volley.bridoire@free.fr</strong><br>Par téléphone: <strong>06 89 55 50 91</strong><br>
-					<br><br>Nous rencontrer: Quand nous n'avons de match &agrave; l'ext&eacute;rieur nous nous entra&icirc;nons le <strong>mardi soir à 20h30</strong> au <a href="gymnase.html"><b>Gymnase de Rochassieux à La Bridoire</b></a>.</p>
-                    
+                    <p class="mbr-section-lead lead">
+Vous pouvez nous contacter:<br>
+Par email: <strong>volley.bridoire@free.fr</strong><br>
+Par téléphone: <strong>06 89 55 50 91</strong><br>
+<br><br>
+Nous rencontrer: Quand nous n'avons de match &agrave; l'ext&eacute;rieur nous nous entra&icirc;nons le
+<strong>mardi soir à 20h30</strong> au <a href="gymnase.php"><b>Gymnase de Rochassieux à La Bridoire</b></a>.
+</p>
                 </div>
             </div>
         </div>
     </div>
 
-    
-
 </section>
 
-	<?php include_once 'inc/footer.html';?>
+   <?php include_once 'inc/footer.html';?>
 
-	<?php include_once 'inc/jsscripts.html';?>
+   <?php include_once 'inc/jsscripts.html';?>
 
   <input name="animation" type="hidden">
   </body>

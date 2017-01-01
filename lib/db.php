3fb2988fd66da970a8cfd142a62b9d8d0933ae92
Olivier Korach <olivier.korach@gmail.com>
2017-01-01 11:51:27 +0100

    open and close DB from class

--- lib/db.php
+++ lib/db.php
@@ -21,7 +21,7 @@
 
 const DB_SERVER = 'sql.free.fr';
 const DB_NAME   = 'volley.bridoire';
-const DB_PASSWD = 'lhassa';
+const DB_PASSWD = 'lhassa73';
 
 //==============================================================================
 //

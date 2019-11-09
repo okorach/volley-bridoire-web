f6965d817dea3295b7e2f0fb694432b704592219
Olivier Korach <olivier.korach@gmail.com>
2019-11-09 22:33:39 +0100

    Amélioration position GPS du gymanse

--- gymnase/gymnase.php
+++ gymnase/gymnase.php
@@ -16,7 +16,7 @@
             <h1 class="mbr-section-title display-1">Ou nous retrouver ?</h1>
             <p class="mbr-section-lead lead"><br>
 			Nous nous entrainons et nous jouons nos matchs à domicile<br>le mardi soir à partir de 20h30<br>au
-			<a href="https://www.google.fr/maps/dir/Chamb%C3%A9ry/45.5334845,5.7417367/@45.5308466,5.7361098,15z/data=!4m8!4m7!1m5!1m1!1s0x478ba85708695f63:0xd5c21acfa63d899c!2m2!1d5.917781!2d45.564601!1m0?hl=fr" target="_blank"><b>Gymnase de Rochassieux &agrave; La Bridoire</b></a>.
+			<a href="https://www.google.fr/maps/place/Salle+polyvalente+de+La+Bridoire/@45.5306996,5.7410314,17z/data=!4m5!3m4!1s0x0:0x5fd9c80d53c4e136!8m2!3d45.5335369!4d5.7418598" target="_blank"><b>Gymnase de Rochassieux &agrave; La Bridoire</b></a>.
 			<br>L'hiver il nous arrive de jouer à la<br> <a href="https://www.google.fr/maps/dir/Chamb%C3%A9ry/45.553073,5.7826093/@45.5516486,5.7770732,17z/data=!3m1!4b1!4m9!4m8!1m5!1m1!1s0x478ba85708695f63:0xd5c21acfa63d899c!2m2!1d5.917781!2d45.564601!1m0!3e0" target="_blank"><b>Salle fran&ccedil;ois Cachoud &agrave; St Alban de Montbel</b></a>.<br>Pour voir ou cela se trouve cliquer sur les lien ci-dessus ou voir les plan ci-dessous.</p>
         </div>
         <div class="row">
@@ -24,7 +24,7 @@
 				<p class="mbr-section-lead lead" align="center"><br>
 					<a href="https://www.google.fr/maps/dir/Chamb%C3%A9ry/45.5334845,5.7417367/@45.5308466,5.7361098,15z/data=!4m8!4m7!1m5!1m1!1s0x478ba85708695f63:0xd5c21acfa63d899c!2m2!1d5.917781!2d45.564601!1m0?hl=fr" target="_blank"><b>Gymnase de Rochassieux &agrave; La Bridoire</b></a>
 				</p>
-                 <div class="mbr-map"><iframe frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0Dx_boXQiwvdz8sJHoYeZNVTdoWONYkU&amp;q=place_id:EjA1MjcgUm91dGUgZGUgbGEgQ29tYmUsIDczNTIwIExhIEJyaWRvaXJlLCBGcmFuY2U" allowfullscreen=""></iframe>
+                 <div class="mbr-map"><iframe frameborder="0" style="border:0" <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.9754474195197!2d5.741031408166508!3d45.530699632197134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5fd9c80d53c4e136!2sSalle%20polyvalente%20de%20La%20Bridoire!5e0!3m2!1sfr!2sfr!4v1573334924228!5m2!1sfr!2sfr" allowfullscreen=""></iframe>" allowfullscreen=""></iframe>
 				</div>
 			</div>
             <div class="col-xs-12 col-md-6 text-xs-center">

b3963d35f93867ceb049619f5f40f140d8abe769
Olivier Korach <olivier.korach@gmail.com>
2020-06-07 16:11:54 +0200

    typo

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if ($nextGame == null) {
       $messageNextGame .= ' à domicile à 20h30,<br>au <a href="gymnase/gymnase.php">gymnase de Rochassieux</a>.';
    }
 }
-$messageNextGame = "Le championnat est d&eacute;finitivement interrompu pour cette ann&eacute;e. #restezchezvous.<br>Rendez-vous &agrace; la mi-ao&uirc;t pour en savoir plus pour le déroulement du championnat 2020-2021"
+$messageNextGame = "Le championnat est d&eacute;finitivement interrompu pour cette ann&eacute;e. #restezchezvous.<br>Rendez-vous &agrace; la mi-ao&ucirc;t pour en savoir plus pour le déroulement du championnat 2020-2021"
 ?>
 
 <!------------------------------ INTRO ---------------------------------->
@@ -34,7 +34,7 @@ $messageNextGame = "Le championnat est d&eacute;finitivement interrompu pour cet
             <div class="mbr-section col-md-10 col-md-offset-1 text-xs-center">
 
             <p class="mbr-section-lead lead">
-            <br>Bienvenue sur notre site. Recontactez nous mi-ao&uirc;t pour le lancement de la saison 2020-2021 !
+            <br>Bienvenue sur notre site. Recontactez nous mi-ao&ucirc;t pour le lancement de la saison 2020-2021 !
             <br>
             <?php echo $messageNextGame ?>
             </p>
